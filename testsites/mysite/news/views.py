bc55539673792dea5f8f027c1c05e776f57f547c
Alexandr <albenzar@bk.ru>
2020-08-03 14:13:19 +0300

    В  ответ на запрос страницы news и news/test возвращаются строки

--- testsites/mysite/news/views.py
+++ testsites/mysite/news/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+# Views-контроллер вызывается в ответ на клиентский запрос. Обрабатывет запрос, формирует данные запрашивая их у модели
+# и возвращает ответ в виде представления заполненного данными. КОнтроллер связуещее звено
+# между данными и их отображениями
+
+def index(requests):
+    # print(requests)
+    return HttpResponse('Hello World')
+
+def test(requests):
+    # print(requests)
+    return HttpResponse('<h1>Тестовая страница</h1>')
